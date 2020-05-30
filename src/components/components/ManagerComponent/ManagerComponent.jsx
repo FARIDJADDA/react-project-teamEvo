94571af7601af6ada4562927cae8531daf02cc88
Hamza Nayyer <hamzanayyer@gmail.com>
2020-05-30 15:24:58 +0200

    fix css in staffList component

--- src/components/components/ManagerComponent/ManagerComponent.jsx
+++ src/components/components/ManagerComponent/ManagerComponent.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import "./ManagerComponent.css";
 const ManagerComponent = ({ data }) => {
   return (
     <>
-      <Col className='d-flex justify-content-center col-4 p-5'>
+      <Col className='d-flex flex-column justify-content-center col-12 col-sm-4 p-5'>
+        <Row className=' d-flex justify-content-center staff-pseudo'>
+          <Col className='col-12'>
+            <p>{data.nickName}</p>
+          </Col>
+        </Row>
         <Card className='cardElement'>
           <Card.Img className='imageManager' variant='top' src={data.image} />
           <Card.Body className='affichage'>
-            <Card.Text>
+            <Card.Text className='text-card'>
               <p>
                 {data.firstName}-{data.lastName}
               </p>
--- src/components/components/Staff/ChiefComponent.jsx
+++ src/components/components/Staff/ChiefComponent.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "./ChiefComponent.css";
-import { Col } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 const ChiefComponent = ({ data }) => {
   return (
-    <Col className='d-flex justify-content-center offset-2 col-3 card-border'>
+    <Col className='d-flex flex-column justify-content-center offset-2 col-3 card-border'>
+      <Row className=' d-flex justify-content-center staff-pseudo'>
+        <Col className='col-12'>
+          <p>{data.nickName}</p>
+        </Col>
+      </Row>
       <Card className='cardElement'>
         <Card.Img className='imageChief' variant='top' src={data.image} />
         <Card.Body className='affichage'>
-          <Card.Text>
+          <Card.Text className='text-card'>
             <p>
               {data.firstName}-{data.lastName}
             </p>
