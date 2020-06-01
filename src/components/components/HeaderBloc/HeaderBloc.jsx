30a40c51bee37edc67b4eba52d2f37b79fd50140
Valentin Diogo <valentin.diogo@hotmail.fr>
2020-06-01 12:58:46 +0200

    edit: values and visions page, edit HeaderBloc Component

--- src/components/components/HeaderBloc/HeaderBloc.jsx
+++ src/components/components/HeaderBloc/HeaderBloc.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Row } from "react-bootstrap";
-import "./HeaderBloc.css";
+import "./headerBloc.css";
 
-const HeaderBloc = () => {
+const HeaderBloc = ({ background, firstTitle, secondTitle, button }) => {
   return (
-    <Row className="d-flex justify-content-center header-bloc p-5">
-      <div className="header-title">
-        <h1>
-          <span className="hashtag">#</span>WeAre
-          <span className="yellow-title">Evo</span>
-        </h1>
-        <h2>
-          <span className="yellow-title">TOGETHER</span>, WE STAND
-        </h2>
-      </div>
+    <Row
+      className='d-flex flex-column justify-content-center header-title p-5'
+      style={background}>
+      {firstTitle ? firstTitle : ""}
+      {secondTitle ? secondTitle : ""}
+      {button ? (
+        <div className='d-flex justify-content-center p-1'>{button}</div>
+      ) : (
+        ""
+      )}
     </Row>
   );
 };
--- src/components/pages/AboutValuesAndVisions.jsx
+++ src/components/pages/AboutValuesAndVisions.jsx
@@ -1,40 +1,105 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
-import HeaderComponent from "../components/generic/HeaderComponent";
-import ItemList from "../components/generic/ItemList";
 import "../components/generic/style.scss";
+import ValuesItem from "../components/ValuesAndVisions/ValuesItem/ValuesItem";
+import HeaderBloc from "../components/headerBloc/HeaderBloc";
 
-export default () => {
-  const data = useSelector((state) => state.vision);
+const AboutValuesAndVisions = () => {
+  const valuesAndVisions = useSelector((state) => state.vision);
 
-  // use map to create the 'internal layout':
-  const RenderData = () => {
-    return data.map((item) => {
-      return (
-        <>
-          {item.header.button !== undefined ? (
-            <HeaderComponent
-              title={item.header.title}
-              button={item.header.button}
-            />
-          ) : (
-            <HeaderComponent title={item.header.title} />
-          )}
-          {
-            <>
-              <ItemList data={item.content} />
-            </>
-          }
-        </>
-      );
-    });
-  };
   return (
-    <>
-      <Container fluid className="content">
-        <RenderData />
-      </Container>
-    </>
+    <Container fluid>
+      <HeaderBloc
+        background={{
+          backgroundImage: `url('https://firebasestorage.googleapis.com/v0/b/react-project-team-evo.appspot.com/o/valuesImage%2Favaa-01-25.png?alt=media&token=6d21715b-428b-443c-9a2a-6e2f900d0701')`,
+          backgroundSize: "100% 400%",
+          backgroundPosition: "center 20%",
+          borderBottom: "solid 3px #c8d600",
+        }}
+        firstTitle={
+          <h1 style={{ marginTop: 15, fontFamily: "KoratakiW01-Book" }}>
+            Our Values
+          </h1>
+        }
+        button={
+          <button
+            style={{
+              fontWeight: "bold",
+              fontFamily: "bebas Neue",
+              padding: "1px 12px",
+              fontSize: "20px",
+              backgroundColor: "#c8d600",
+              borderStyle: "none",
+            }}>
+            #WEAREEVO
+          </button>
+        }
+      />
+
+      {valuesAndVisions
+        .filter((data) => data.filter === "our values")
+        .map((data) => {
+          return <ValuesItem data={data} />;
+        })}
+
+      <HeaderBloc
+        background={{
+          backgroundImage: `url('https://firebasestorage.googleapis.com/v0/b/react-project-team-evo.appspot.com/o/background-game%2Ffond-valorant.png?alt=media&token=aaffe6b0-9365-476e-930b-c58ed42a0bdf')`,
+          backgroundSize: "100% 250%",
+          backgroundPosition: "center 20%",
+          borderBottom: "solid 3px #c8d600",
+          borderTop: "solid 3px #c8d600",
+        }}
+        firstTitle={
+          <h1 style={{ marginTop: 15, fontFamily: "KoratakiW01-Book" }}>
+            Code of
+          </h1>
+        }
+        secondTitle={
+          <h1
+            style={{
+              marginBottom: 15,
+              color: "#c8d600",
+              fontFamily: "KoratakiW01-Book",
+            }}>
+            Conduct
+          </h1>
+        }
+      />
+
+      {valuesAndVisions
+        .filter((data) => data.filter === "code of conduct")
+        .map((data) => {
+          return <ValuesItem data={data} />;
+        })}
+
+      <HeaderBloc
+        background={{
+          backgroundImage: `url('https://firebasestorage.googleapis.com/v0/b/react-project-team-evo.appspot.com/o/background-game%2Ffond-r6.png?alt=media&token=edf0ee31-8a6a-49a6-a28c-3dcc5a6293be')`,
+          backgroundSize: "100% 250%",
+          backgroundPosition: "center 45%",
+          borderBottom: "solid 3px #c8d600",
+          borderTop: "solid 3px #c8d600",
+        }}
+        firstTitle={
+          <h1
+            style={{
+              margin: "50px 0 50px 0",
+              fontFamily: "KoratakiW01-Book",
+            }}>
+            Our visions
+          </h1>
+        }
+      />
+
+      {valuesAndVisions
+        .filter((data) => data.filter === "our visions")
+        .map((data) => {
+          return <ValuesItem data={data} />;
+        })}
+    </Container>
   );
 };
+
+export default AboutValuesAndVisions;
