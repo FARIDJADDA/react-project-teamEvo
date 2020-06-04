d5436d590299c33aceb45b64e7cd7bf3ff3eabec
djadda Farid <faridjadda750@gmail.com>
2020-06-04 14:08:19 +0200

    edit css on all page

--- src/components/components/players/players.jsx
+++ src/components/components/players/players.jsx
@@ -10,14 +10,14 @@ import {
 
 export default ({ player: { firstName, lastName, role, nationality } }) => {
   return (
-    <Col className='col-6 col-md-4 d-flex justify-content-center'>
-      <Row className='player-container'>
-        <Col className='player-infos'>
-          <div>
+    <Col className="col-6 col-md-4 d-flex justify-content-center">
+      <Row className="player-container">
+        <Col className="player-infos">
+          <div className="text-grey">
             {firstName} {lastName}
           </div>
-          <div>{role}</div>
-          <div>{nationality}</div>
+          <div className="text-yellow">{role}</div>
+          <div className="text-grey">{nationality}</div>
           <div
             style={{
               width: "100%",
@@ -26,15 +26,15 @@ export default ({ player: { firstName, lastName, role, nationality } }) => {
               margin: "4px 0",
             }}
           />
-          <div className='player-social'>
-            <a href='https://twitter.com/TeamEvoAB'>
-              <FontAwesomeIcon className='white' icon={faTwitter} />
+          <div className="player-social">
+            <a href="https://twitter.com/TeamEvoAB">
+              <FontAwesomeIcon className="white" icon={faTwitter} />
             </a>
-            <a href='https://discord.com/' alt='test'>
-              <FontAwesomeIcon className='white' icon={faYoutube} />
+            <a href="https://discord.com/" alt="test">
+              <FontAwesomeIcon className="white" icon={faYoutube} />
             </a>
-            <a href='https://fr.linkedin.com/company/team-evo-ab' alt='test'>
-              <FontAwesomeIcon className='white' icon={faTwitch} />
+            <a href="https://fr.linkedin.com/company/team-evo-ab" alt="test">
+              <FontAwesomeIcon className="white" icon={faTwitch} />
             </a>
           </div>
         </Col>
--- src/containers/Cdpr/Cdpr.jsx
+++ src/containers/Cdpr/Cdpr.jsx
@@ -3,30 +3,27 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./Cdpr.css";
 import { useSelector } from "react-redux";
 import CdprComponent from "../../components/components/Cdpr/CdprComponent";
-import WaitData from "../../components/components/Spinner/WaitData"
+import WaitData from "../../components/components/Spinner/WaitData";
 
 const Cdpr = () => {
   const cdpr = useSelector((state) => state.cdpr);
 
   if (!cdpr.length) {
-    return (
-      <WaitData />
-    );
+    return <WaitData />;
   }
   return (
-    
     <>
       <Container fluid>
         <Row>
-          <Col className='headerCdpr p-5'>
+          <Col className="headerCdpr p-5">
             <p>Team Evo</p>
-            <p className='yellowColor'>Privacy Policy</p>
+            <p className="yellowColor">Privacy Policy</p>
           </Col>
         </Row>
       </Container>
       <Container>
         <Row>
-          <Col>
+          <Col className="pt-3">
             {cdpr.map((element) => {
               return <CdprComponent data={element} />;
             })}
