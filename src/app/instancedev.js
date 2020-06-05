c49befe366118f054050939ac7c990a992ff4692
Hamza Nayyer <hamzanayyer@gmail.com>
2020-06-05 19:04:55 +0200

    build app and deploy

--- src/app/instancedev.js
+++ src/app/instancedev.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/data",
-  timeout: 1000,
+  baseURL: "data",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
   },
 });
 
-// todo blabla
 export default api;
