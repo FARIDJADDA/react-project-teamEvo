f3742e3ac9aa64e8ac1caa4a2c67cefde7f7dffa
djadda Farid <faridjadda750@gmail.com>
2020-06-06 23:11:37 +0200

    edit css

--- src/components/structure/navbar/NavBar.jsx
+++ src/components/structure/navbar/NavBar.jsx
@@ -18,25 +18,30 @@ const FullNavBar = () => {
   const navs = useSelector((state) => state.navs);
   return (
     <>
-      <Navbar className='navbarcustom' id='navbar' variant='dark' expand='lg'>
-        <Navbar.Brand className='white' href='/home'>
+      <Navbar
+        className="navbarcustom fixed-top"
+        id="navbar"
+        variant="dark"
+        expand="lg"
+      >
+        <Navbar.Brand className="white" href="/home">
           <img
-            className='logo-rond'
-            alt='logo-rond'
-            src='https://firebasestorage.googleapis.com/v0/b/projet-teamevo.appspot.com/o/imageEvo%2Flogo-evo-rond.png?alt=media&token=284e03a4-b60f-4264-8728-5a24eceeabaa'
+            className="logo-rond"
+            alt="logo-rond"
+            src="https://firebasestorage.googleapis.com/v0/b/projet-teamevo.appspot.com/o/imageEvo%2Flogo-evo-rond.png?alt=media&token=284e03a4-b60f-4264-8728-5a24eceeabaa"
           />
           <img
-            className='logo-evo'
-            alt='logo-evo'
-            src='https://firebasestorage.googleapis.com/v0/b/projet-teamevo.appspot.com/o/imageEvo%2Flogo-teamevo.png?alt=media&token=ba39d186-ba00-4912-8a73-33b833c9ff37'
+            className="logo-evo"
+            alt="logo-evo"
+            src="https://firebasestorage.googleapis.com/v0/b/projet-teamevo.appspot.com/o/imageEvo%2Flogo-teamevo.png?alt=media&token=ba39d186-ba00-4912-8a73-33b833c9ff37"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto white'>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto white">
             {navs.map((route, index) => {
               return !route.children ? (
-                <NavLink className='white navlink' to={route.url} key={index}>
+                <NavLink className="white navlink" to={route.url} key={index}>
                   {route.text}
                 </NavLink>
               ) : (
@@ -46,41 +51,46 @@ const FullNavBar = () => {
           </Nav>
           <div style={{ display: "flex" }}>
             <Nav.Link
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://twitter.com/TeamEvoAB'>
-              <FontAwesomeIcon className='white' icon={faTwitter} />
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://twitter.com/TeamEvoAB"
+            >
+              <FontAwesomeIcon className="white" icon={faTwitter} />
             </Nav.Link>
             <Nav.Link
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://discord.com/'
-              alt='test'>
-              <FontAwesomeIcon className='white' icon={faDiscord} />
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://discord.gg/dXurMUF"
+              alt="test"
+            >
+              <FontAwesomeIcon className="white" icon={faDiscord} />
             </Nav.Link>
             <Nav.Link
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://fr.linkedin.com/company/team-evo-ab'
-              alt='test'>
-              <FontAwesomeIcon className='white' icon={faLinkedin} />
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://fr.linkedin.com/company/team-evo-ab"
+              alt="test"
+            >
+              <FontAwesomeIcon className="white" icon={faLinkedin} />
             </Nav.Link>
             <Nav.Link
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://www.youtube.com/channel/UCNk4BPBPEE5fwJPqZPq_Drg'
-              alt='test'>
-              <FontAwesomeIcon className='white' icon={faYoutube} />
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.youtube.com/"
+              alt="test"
+            >
+              <FontAwesomeIcon className="white" icon={faYoutube} />
             </Nav.Link>
             <Nav.Link
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://www.twitch.tv/team/evo'
-              alt='test'>
-              <FontAwesomeIcon className='white' icon={faTwitch} />
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.twitch.tv/teamevotv"
+              alt="test"
+            >
+              <FontAwesomeIcon className="white" icon={faTwitch} />
             </Nav.Link>
-            <Nav.Link href='#' alt='test'>
-              <strong className='white'>FR</strong>
+            <Nav.Link href="#" alt="test">
+              <strong className="white">FR</strong>
             </Nav.Link>
           </div>
         </Navbar.Collapse>
