68e54a70dd7b16a814f57af292f3fa827838e51f
Valentin Diogo <valentin.diogo@hotmail.fr>
2020-06-07 19:24:39 +0200

    edit: grid on partners page

--- src/components/components/Partners/Partners.jsx
+++ src/components/components/Partners/Partners.jsx
@@ -10,112 +10,120 @@ const PartnerComponent = ({ partner }) => {
 
   return partner.position % 2 === 0 ? (
     <Row
-      className="bloc-partner p-5 bg-first"
-      style={{ backgroundImage: `url("${partner.bgImage}")` }}
-    >
-      <Col className="offset-2 col-8">
-        <Row className="d-flex justify-content-center">
-          <Col>
-            <div className="f-flex justify-content-center partner-links">
+      className='bloc-partner bg-first justify-content-md-center'
+      style={{ backgroundImage: `url("${partner.bgImage}")` }}>
+      <Col md={12} lg={8}>
+        <Row>
+          <Col md={12} lg={7} className='justify-content-center'>
+            <Row className='partner-links justify-content-center'>
               <a
-                target="_blank"
-                rel="noopener noreferrer"
-                className="partner-website"
-                href={partner.website}
-              >
+                target='_blank'
+                rel='noopener noreferrer'
+                className='partner-website'
+                href={partner.website}>
                 {partner.website}
               </a>
               <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={partner.twitter}
-              >
-                <FontAwesomeIcon className="social-partner" icon={faTwitter} />
+                target='_blank'
+                rel='noopener noreferrer'
+                href={partner.twitter}>
+                <FontAwesomeIcon className='social-partner' icon={faTwitter} />
               </a>
               <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={partner.facebook}
-              >
-                <FontAwesomeIcon className="social-partner" icon={faFacebook} />
+                target='_blank'
+                rel='noopener noreferrer'
+                href={partner.facebook}>
+                <FontAwesomeIcon className='social-partner' icon={faFacebook} />
               </a>
-            </div>
+            </Row>
           </Col>
-          <Col className="bloc-logo-right">
-            <button
-              className="btn-link"
-              onClick={() => setOpen(!open)}
-              aria-controls="example-collapse-text"
-              aria-expanded={open}
-            >
-              <img
-                src={partner.image}
-                alt="logo-partner"
-                className="logo-partner"
-              />
-            </button>
+          <Col md={12} lg={5}>
+            <Row className='justify-content-center row-img'>
+              <button
+                className='btn-link'
+                onClick={() => setOpen(!open)}
+                aria-controls='example-collapse-text'
+                aria-expanded={open}>
+                <img
+                  src={partner.image}
+                  alt='logo-partner'
+                  className='logo-partner'
+                />
+              </button>
+            </Row>
+          </Col>
+          <Col md={12} lg={12}>
+            <Collapse in={open}>
+              <Row id='example-collapse-text'>
+                <PartnerDetail partner={partner} />
+              </Row>
+            </Collapse>
           </Col>
         </Row>
-        <Collapse in={open}>
-          <Row id="example-collapse-text" className="pt-5">
-            <PartnerDetail partner={partner} />
-          </Row>
-        </Collapse>
       </Col>
     </Row>
   ) : (
     <Row
-      className="bloc-partner2 p-5"
-      style={{ backgroundImage: `url("${partner.bgImage}")` }}
-    >
-      <Col className="offset-2 col-8">
-        <Row className="d-flex justify-content-center">
-          <Col className="bloc-logo-left">
-            <button
-              className="btn-link"
-              onClick={() => setOpen(!open)}
-              aria-controls="example-collapse-text"
-              aria-expanded={open}
-            >
-              <img
-                src={partner.image}
-                alt="logo-partner"
-                className="logo-partner"
-              />
-            </button>
+      className='bloc-partner2 justify-content-md-center'
+      style={{ backgroundImage: `url("${partner.bgImage}")` }}>
+      <Col md={12} lg={8}>
+        <Row>
+          <Col md={12} lg={5}>
+            <Row className='justify-content-center row-img'>
+              <button
+                className='btn-link'
+                onClick={() => setOpen(!open)}
+                aria-controls='example-collapse-text'
+                aria-expanded={open}>
+                <img
+                  src={partner.image}
+                  alt='logo-partner'
+                  className='logo-partner'
+                />
+              </button>
+            </Row>
           </Col>
-          <Col>
-            <div className="f-flex justify-content-center partner-links">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                className="partner-website"
-                href={partner.website}
-              >
-                {partner.website}
-              </a>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={partner.twitter}
-              >
-                <FontAwesomeIcon className="social-partner" icon={faTwitter} />
-              </a>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={partner.facebook}
-              >
-                <FontAwesomeIcon className="social-partner" icon={faFacebook} />
-              </a>
-            </div>
+          <Col md={12} lg={7} className='justify-content-center'>
+            <Row className='partner-links justify-content-center'>
+              <Col md={12} lg='auto' className='text-center'>
+                <a
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='partner-website'
+                  href={partner.website}>
+                  {partner.website}
+                </a>
+              </Col>
+              <Col md={12} lg='auto' className='text-center'>
+                <a
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href={partner.twitter}>
+                  <FontAwesomeIcon
+                    className='social-partner'
+                    icon={faTwitter}
+                  />
+                </a>
+                <a
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href={partner.facebook}>
+                  <FontAwesomeIcon
+                    className='social-partner'
+                    icon={faFacebook}
+                  />
+                </a>
+              </Col>
+            </Row>
+          </Col>
+          <Col md={12} lg={12}>
+            <Collapse in={open}>
+              <Row id='example-collapse-text'>
+                <PartnerDetail partner={partner} />
+              </Row>
+            </Collapse>
           </Col>
         </Row>
-        <Collapse in={open}>
-          <Row id="example-collapse-text" className="pt-5">
-            <PartnerDetail partner={partner} />
-          </Row>
-        </Collapse>
       </Col>
     </Row>
   );
