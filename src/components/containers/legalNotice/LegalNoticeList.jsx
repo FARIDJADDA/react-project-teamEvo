ccd8b361623c5aa5b01bc7b9e2c5635059b64fce
Valentin Diogo <valentin.diogo@hotmail.fr>
2020-06-08 17:46:27 +0200

    edit: grid boostrap

--- src/components/containers/legalNotice/LegalNoticeList.jsx
+++ src/components/containers/legalNotice/LegalNoticeList.jsx
@@ -1,24 +1,36 @@
-import { useSelector } from "react-redux";
 import React from "react";
+import { useSelector } from "react-redux";
 import { Container } from "react-bootstrap";
-import HeaderComponent from "../../components/legalNotice/HeaderComponent";
-import ItemList from "../../components/legalNotice/ItemList";
-import "../../components/legalNotice/style.css";
-import WaitData from "../../components/Spinner/WaitData";
+
+import HeaderBloc from "./../../components/HeaderBloc/HeaderBloc";
+import LegalNoticeItem from "./../../components/LegalNotice/LegalNoticeItem";
 
 const LegalNoticeList = () => {
-  const data = useSelector((state) => state.legal);
-
-  if (!data.length) {
-    return <WaitData />;
-  }
+  const legal = useSelector((state) => state.legal);
   return (
-    <>
-      <Container fluid className='legal'>
-        <HeaderComponent title='Legal Notice' />
-        <ItemList data={data} />
-      </Container>
-    </>
+    <Container fluid>
+      <HeaderBloc
+        background={{
+          backgroundImage: `url('https://firebasestorage.googleapis.com/v0/b/react-project-team-evo.appspot.com/o/HomeImage%2Favaa-06-25.png?alt=media&token=a50f9414-1697-4dac-b033-96f14e320b19')`,
+          backgroundSize: "100% 350%",
+          backgroundPosition: "center 40%",
+          borderBottom: "solid 3px #c8d600",
+        }}
+        firstTitle={
+          <h1
+            style={{
+              marginTop: 30,
+              marginBottom: 30,
+              fontFamily: "KoratakiW01-Book",
+            }}>
+            Legal Notice
+          </h1>
+        }
+      />
+      {legal.map((data) => {
+        return <LegalNoticeItem data={data} />;
+      })}
+    </Container>
   );
 };
 
